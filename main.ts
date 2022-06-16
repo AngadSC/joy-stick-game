b59f639fcc35fe31ddbe66e314c5ca94f23aef83
AngadSC <104170661+AngadSC@users.noreply.github.com>
2022-06-15 21:00:48 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
+function drop_led () {
+	
+}
 let joyZ = 0
 let joyX = 0
 let joyY = 0
+game.setLife(10)
 let catcher = game.createSprite(2, 4)
 let ball = game.createSprite(2, 0)
 game.setScore(0)
 ball.set(LedSpriteProperty.Blink, 3)
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        ball.change(LedSpriteProperty.Y, 1)
+        basic.pause(500)
+    }
+    ball.set(LedSpriteProperty.Y, 0)
+    ball.set(LedSpriteProperty.X, randint(0, 4))
+})
 basic.forever(function () {
     joyY = pins.analogReadPin(AnalogPin.P2)
     joyX = pins.analogReadPin(AnalogPin.P0)
@@ -22,5 +34,18 @@ basic.forever(function () {
     }
     if (joyZ < 200) {
         game.setScore(0)
+    }
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (ball.isTouching(catcher)) {
+        game.addScore(1)
+    } else if (ball.get(LedSpriteProperty.Y) == 4) {
+        game.removeLife(0.1)
     }
 })
